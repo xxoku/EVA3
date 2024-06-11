58c3978a579794c335a5dd1037edde2f4131dd46
xoku <129918652+xxoku@users.noreply.github.com>
2024-06-11 11:37:06 -0400

    fin parte 1
    Se modifica nuevamente el index agregando 2 botones que son para la fuente y el contraste de la pagina tambien se agrega mas al style.css para que la pagina se vea mucho mas ordenada. Al script se le agregan para sacar los valores y elementos del formulario (aun no terminado) y empieza avanzar para hacer el contraste.

---
+++ js/script.js
@@ -0,0 +1,33 @@
+window.addEventListener("load",()=>{
+    document.getElementById 
+})
+
+
+//c
+function contraste(){
+    let body = document.body
+    body.classList.toggle(contraste)
+} 
+
+
+
+
+registrar();
+    // recuperar elemento
+    let eNombre = document.getElementById("name");
+    let eApellido = document.getElementById("apellido")
+    let eRut = document.getElementById("rut")
+    let eEmail = document.getElementById("email")
+    let eDireccion = document.getElementById("direccion")
+    let eCodpostal = document.getElementById("codpostal")
+    let eFono = document.getElementById("telefono")
+    let eAsunto = document.getElementById("asunto")
+    //recuperar valor 
+    let vNombre = eNombre.value
+    let vApellido = eApellido.value 
+    let vRut = eRut.value 
+    let vEmail = eEmail.value
+    let vDireccion = eDireccion.value 
+    let vCodpostal = eCodpostal.value 
+    let vFono = eFono.value
+    let vAsunto = eAsunto.value 
